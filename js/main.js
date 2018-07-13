e3c56fe86a6d5af4fed07d2d05568974c06b20c1
kurbatovbn <warkb@yandex.ru>
2018-07-13 09:48:09 +0400

    Добавил контакты. Относительно стабильное состояние

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     new WOW().init();
     $('.header').height($(window).height());
-    $('#portfolio, #about').height($(window).height());
+    $('#portfolio, #about, #contacts').height($(window).height());
 
     $(".navbar a, .header button").click(function(){
     $("body,html").animate({
